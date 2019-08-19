a09fa3b144b28a5222b133fd94b8d97999a4b4cb
earthtone82 <antonio.hubilla@pizzahutaustralia.com.au>
2019-08-19 11:32:24 +1000

    Git Ignore Built Lib File for CLI

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 const chalk = require('chalk')
 const generate = require('./lib')
-// const generateList = require('../lib/generate_list')
-const list = generate()
 
+// generate function is packaged by
+// rollup and transpiled into lib file
+// allowing cli functionality and
+// module functionality to stay separate
+
+const list = generate()
 console.log(`${chalk.cyan(list[0])} ${chalk.yellow(list[1])} ${chalk.red(list[2])} ${chalk.green(list[3])}`)
